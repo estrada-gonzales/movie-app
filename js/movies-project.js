9885df8de31f44e69840e917ee8a6056ef3c2800
javier estrada <estrada.javier.2022@gmail.com>
2021-03-01 16:43:21 -0600

    updating

--- js/movies-project.js
+++ js/movies-project.js
@@ -19,6 +19,7 @@ function getMovies() {
                 moviesHtml += '<li class="list-group-item">Title: ' + movie.title + '</li>';
                 moviesHtml += '<li class="list-group-item">Id: ' + movie.id + '</li>';
                 moviesHtml += '<li class="list-group-item">Rating: ' + movie.rating + '</li>';
+                moviesHtml += '<button id="delete-button">Delete</button>
                 moviesHtml += '</ul>'
                 moviesHtml += '</div>';
                 output += moviesHtml;
@@ -46,11 +47,57 @@ function postMovie(movieObject) {
 }
 
 
+
+
 $("#postAMovie").click(function (e) {
     e.preventDefault();
     postMovie({
         "title": $("#movieTitle").val(),
         "rating": $("#movieRating").val()
         // id: $("#movieId").val()
+    })
+});
+
+function editMovie(movieObject) {
+    const options = {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieObject)
+    };
+    return fetch(movieList, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            getMovies();
+        })
+        .catch(error => console.error(error));
+}
+
+
+function deleteMovie(movieObject) {
+    const options = {
+        method: 'DELETE',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(movieObject)
+    };
+    return fetch(movieList, options)
+        .then(response => response.json())
+        .then(data => {
+            console.log(data)
+            getMovies();
+        })
+        .catch(error => console.error(error));
+}
+
+$("#delete-button").click(function (e) {
+    e.preventDefault();
+    deleteMovie({
+        "title": $("#movieTitle").val(),
+        "rating": $("#movieRating").val(),
+        id: $("#movieId").val()
     })
 });
